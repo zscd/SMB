97d02d3543e28ff321c46ef81fbf72b5b8e860d1
Zsk <zskcd@qq.com>
2018-03-24 18:54:23 +0800

    mpcharttest

---
+++ app/src/main/java/com/example/zsk/smb/fragments/SmbHomeFragment.java
@@ -0,0 +1,24 @@
+package com.example.zsk.smb.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.zsk.smb.R;
+
+/**
+ * Created by zsk on 2018/3/24.
+ */
+
+public class SmbHomeFragment extends Fragment {
+    private View view;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        view = inflater.inflate(R.layout.smb_home_fragment,container,false);
+        return view;
+    }
+
+}
+
