2bc8d5a58f1a7696757b676befd79a1fc8d73ab3
vicsun_dellPC <zxbill@yeah.net>
2018-03-26 16:17:41 +0800

    1.添加了Tablayout的编译支持 2.增加图标 3.增加底部导航栏

---
+++ app/src/main/java/com/example/zsk/smb/DataGenerator.java
@@ -0,0 +1,43 @@
+package com.example.zsk.smb;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by vicsun on 2018/3/5.
+ */
+
+public class DataGenerator {
+
+    public static final int []mTabRes = new int[]{R.drawable.tab_home_selector,R.drawable.tab_discovery_selector,R.drawable.tab_attention_selector,R.drawable.tab_profile_selector};
+    public static final int []mTabResPressed = new int[]{R.drawable.ic_tab_strip_icon_feed_selected,R.drawable.ic_tab_strip_icon_category_selected,R.drawable.ic_tab_strip_icon_pgc_selected,R.drawable.ic_tab_strip_icon_profile_selected};
+    public static final String[]mTabTitle = new String[]{"首页","服务","统计","我的"};
+
+    public static Fragment[] getFragments(String from){
+        Fragment fragments[] = new Fragment[4];
+        fragments[0] = HomeFragment.newInstance(from);
+        fragments[1] = ServiceFragment.newInstance(from);
+        fragments[2] = StatisticsFragment.newInstance(from);
+        fragments[3] = ProfileFragment.newInstance(from);
+        return fragments;
+    }
+
+    /**
+     * 获取Tab 显示的内容
+     * @param context
+     * @param position
+     * @return
+     */
+    public static View getTabView(Context context, int position){
+        View view = LayoutInflater.from(context).inflate(R.layout.home_tab_content,null);
+        ImageView tabIcon = (ImageView) view.findViewById(R.id.tab_content_image);
+        tabIcon.setImageResource(DataGenerator.mTabRes[position]);
+        TextView tabText = (TextView) view.findViewById(R.id.tab_content_text);
+        tabText.setText(mTabTitle[position]);
+        return view;
+    }
+}
