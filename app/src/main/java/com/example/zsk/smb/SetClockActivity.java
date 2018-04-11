0386c806aa28c951aa3eed8595358633d212d09e
vicsun_dellPC <zxbill@yeah.net>
2018-04-11 18:51:18 +0800

    UI优化

---
+++ app/src/main/java/com/example/zsk/smb/SetClockActivity.java
@@ -0,0 +1,11 @@
+package com.example.zsk.smb;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by vicsun on 2018/4/11.
+ */
+
+public class SetClockActivity extends AppCompatActivity {
+
+}
--- app/src/main/java/com/example/zsk/smb/fragments/ServiceFragment.java
+++ app/src/main/java/com/example/zsk/smb/fragments/ServiceFragment.java
@@ -1,5 +1,6 @@
 package com.example.zsk.smb.fragments;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 import android.os.Bundle;
@@ -8,7 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.zsk.smb.BottomTabLayoutActivity;
 import com.example.zsk.smb.R;
+import com.example.zsk.smb.SetClockActivity;
 
 /**
  * Created by zsk on 2018/3/26.
@@ -41,6 +44,37 @@ public class ServiceFragment extends Fragment{
         //textView.setText(mFrom);
         //content.setText("ProfileFragment");
         return view;
+    }
+
+    class MyClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View V) {
+            if (V.getId() == R.id.Activity_AllFunctions_ImageView_Function1) {
+                Intent intent = new Intent(getContext(), SetClockActivity.class);
+                startActivity(intent);
+
+            } else if (V.getId() == R.id.Activity_AllFunctions_ImageView_Function2) {
+
+                //Intent intent = new Intent(getContext(), );
+                //startActivity(intent);
+            } else if (V.getId() == R.id.Activity_AllFunctions_ImageView_Function3) {
+                //Intent intent = new Intent(getContext(), release_message_main.class);
+                //startActivity(intent);
+            } else if (V.getId() == R.id.Activity_AllFunctions_ImageView_Function4) {
+
+                //Intent intent = new Intent(getContext(), t2.class);
+                //startActivity(intent);
+            } else if (V.getId() == R.id.Activity_AllFunctions_ImageView_Function5) {
+                //Intent intent = new Intent(getContext(), Activity_Matk_Mainpage.class);
+                //startActivity(intent);
+            } else if (V.getId() == R.id.Activity_AllFunctions_ImageView_Function6) {
+
+                //Intent intent = new Intent(getContext(), t3.class);
+                //startActivity(intent);
+            }
+        }
+
+
     }
 
 }
